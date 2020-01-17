1494635ab30bada64b73d82875982b76bd98198a
NielzosFilms <nielzosfilms@gmail.com>
2020-01-17 17:31:10 +0100

    betting field complete

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function generateFields(){
         for(var k = 1;k<=3;k++){
             number++;
             table_row.innerHTML += "<td class='betting_field' id='betting_field_R"+r+"K"+k+"'>"+number+"</td>";
-
             color_change++;
             if(color_change > 10){
                 color_change = 0;
@@ -24,8 +23,14 @@ function generateFields(){
             }else if(number == 19){
                 color_1 = "black";
                 color_2 = "red";
+            }else if(number == 36){
+                table.innerHTML += "<tr id='betting_field_table_row_"+(r+1)+"'></tr>";
+                table_row = document.getElementById("betting_field_table_row_"+(r+1));
+                for(var i = 1;i<=3;i++){
+                    table
+                    table_row.innerHTML += "<td class='betting_field' id='2_to_1_R"+r+"K"+k+"'>2to1</td>";
+                }
             }
-
             if(number%2 == 0){
                 document.getElementById("betting_field_R"+r+"K"+k).style.backgroundColor = color_1;
             }else{
